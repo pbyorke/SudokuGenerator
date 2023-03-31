16b53134f2628970973179f87423f66b27950f8f
Peter B Yorke <pbyorke@gmail.com>
2023-03-30 17:55:38 -0700

    Initial Commit

---
+++ SudokuGenerator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SudokuGenerator
+//
+//  Created by Peter Yorke on 3/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SudokuGenerator/SudokuGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuGeneratorApp.swift
+//  SudokuGenerator
+//
+//  Created by Peter Yorke on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SudokuGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
